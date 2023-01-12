3bb9be5ffe0a1e8566cb04db7c0f4a091fee8191
Adonys Santos <adonyssantosjimenez@gmail.com>
2023-01-12 16:55:18 -0400

    Add API spies tests

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,10 @@
 const BooksService = require('./books.service');
 
+// Fake
 const BOOKS_MOCK = [
   {
     _id: '5e6b9e7e7b6d1d2a58c7b5a1',
-    name: 'Harry Potter and the Sorcerer\'s stone',
+    name: "Harry Potter and the Sorcerer's stone",
   },
   {
     _id: '5e6b9e7e7b6d1d2a58c7b5a2',
@@ -15,12 +16,15 @@ const BOOKS_MOCK = [
   },
 ];
 
-const MongoLibStub = {
-  getAll: () => Promise.resolve(BOOKS_MOCK),
-  create: () => Promise.resolve(BOOKS_MOCK[0]),
-};
+// Spies
+const mockGetAll = jest.fn();
 
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+// Stubs
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => Promise.resolve(BOOKS_MOCK[0]),
+})));
 
 describe('Tests for BooksService', () => {
   let service;
@@ -30,41 +34,46 @@ describe('Tests for BooksService', () => {
     jest.clearAllMocks();
   });
 
-  describe('Tests for BooksService.getBooks', () => {
-    describe('getBooks', () => {
-      let books;
+  describe('getBooks', () => {
+    let books;
 
-      beforeEach(async () => {
-        books = await service.getBooks();
-      });
-
-      test('should return an array of books', async () => {
-        expect(books).toBeInstanceOf(Array);
-      });
-
-      test('should return a list of 3 books', async () => {
-        expect(books.length).toBe(3);
-      });
-
-      test('should return a list of the BOOKS_MOCK', async () => {
-        expect(books).toEqual(BOOKS_MOCK);
-      });
+    beforeEach(async () => {
+      mockGetAll.mockResolvedValue(BOOKS_MOCK);
+      books = await service.getBooks({});
     });
 
-    describe('createBook', () => {
-      let book;
+    test('should return a Array instance data', async () => {
+      expect(books).toBeInstanceOf(Array);
+    });
 
-      beforeEach(async () => {
-        book = await service.createBook(BOOKS_MOCK[0]);
-      });
+    test('should return a list of 3 books', async () => {
+      expect(books.length).toBe(3);
+    });
 
-      test('should create object book', async () => {
-        expect(book).toBeInstanceOf(Object);
-      });
+    test('should return a list of the BOOKS_MOCK', async () => {
+      expect(books).toEqual(BOOKS_MOCK);
+    });
 
-      test('should create a book with name Harry Potter and the Sorcerer\'s stone', async () => {
-        expect(book.name).toBe('Harry Potter and the Sorcerer\'s stone');
-      });
+    test('should been called getAll method one time and with the params', async () => {
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
     });
   });
+
+  // describe('createBook', () => {
+  //   let book;
+
+  //   beforeEach(async () => {
+  //     book = await service.createBook(BOOKS_MOCK[0]);
+  //   });
+
+  //   test('should create object book', async () => {
+  //     expect(book).toBeInstanceOf(Object);
+  //   });
+
+  //   test("should create a book with name Harry Potter and the Sorcerer's stone", async () => {
+  //     expect(book.name).toBe("Harry Potter and the Sorcerer's stone");
+  //   });
+  // });
 });
